acf5b0505a3c1edb3b269d49e8590c321bfe6f74
Eric <erjohnson.pdx@gmail.com>
2014-08-23 15:07:57 -0700

    Fix plural error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   match('contacts', {:via => :get, :to => 'contacts#index'})
-  match('contacts/:id', {:via => :get, :to => 'contact#show'})
+  match('contacts/:id', {:via => :get, :to => 'contacts#show'})
 end
