56681dc711dd477b937ad8d2b88c98995c0ce7d8
Eric <erjohnson.pdx@gmail.com>
2014-08-23 15:19:43 -0700

    Add error to new contact route

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,15 +4,19 @@ class ContactsController < ApplicationController
     render('contacts/index.html.erb')
   end
 
-  def create
-    @contact = Contact.create(:name => params[:name],
-                              :email => params[:email],
-                              :phone => params[:phone])
-    render('contacts/success.html.erb')
-  end
-
   def new
     render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+                           :email => params[:email],
+                           :phone => params[:phone])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
   end
 
   def show
