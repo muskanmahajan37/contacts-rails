0b300206bfe6d6de6acb14728bbd128e9e218910
Eric <erjohnson.pdx@gmail.com>
2014-08-23 14:52:17 -0700

    Update contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,10 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
+    render('contacts/index.html.erb')
+  end
+
+  def show
     @contacts = Contact.find(params[:id])
     render('contacts/show.html.erb')
   end
